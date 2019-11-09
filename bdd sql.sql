4e5c1fbf9e8f657d4e0d472b00828907029ae1bc
AntTBD <47208483+AntTBD@users.noreply.github.com>
2019-11-09 22:41:08 +0100

    bdd v5
    suppression de la reprise de l'id_utilisateur pour les candidats et les entreprises

--- bdd sql.sql
+++ bdd sql.sql
@@ -57,13 +57,12 @@ CREATE TABLE utilisateurs(
 #------------------------------------------------------------
 
 CREATE TABLE candidats(
-        id_utilisateurs Int NOT NULL ,
-        id              Int NOT NULL ,
-        prenom          Varchar (255) ,
-        civilite        Varchar (4)
-	,CONSTRAINT candidats_PK PRIMARY KEY (id_utilisateurs,id)
+        id       Int NOT NULL ,
+        prenom   Varchar (255) ,
+        civilite Varchar (4)
+	,CONSTRAINT candidats_PK PRIMARY KEY (id)
 
-	,CONSTRAINT candidats_utilisateurs_FK FOREIGN KEY (id_utilisateurs) REFERENCES utilisateurs(id)
+	,CONSTRAINT candidats_utilisateurs_FK FOREIGN KEY (id) REFERENCES utilisateurs(id)
 )ENGINE=InnoDB;
 
 
@@ -72,13 +71,12 @@ CREATE TABLE candidats(
 #------------------------------------------------------------
 
 CREATE TABLE entreprises(
-        id_utilisateurs Int NOT NULL ,
-        id              Int NOT NULL ,
-        siteInternet    Varchar (255) ,
-        description     Varchar (1500)
-	,CONSTRAINT entreprises_PK PRIMARY KEY (id_utilisateurs,id)
+        id           Int NOT NULL ,
+        siteInternet Varchar (255) ,
+        description  Varchar (1500)
+	,CONSTRAINT entreprises_PK PRIMARY KEY (id)
 
-	,CONSTRAINT entreprises_utilisateurs_FK FOREIGN KEY (id_utilisateurs) REFERENCES utilisateurs(id)
+	,CONSTRAINT entreprises_utilisateurs_FK FOREIGN KEY (id) REFERENCES utilisateurs(id)
 )ENGINE=InnoDB;
 
 
@@ -87,20 +85,19 @@ CREATE TABLE entreprises(
 #------------------------------------------------------------
 
 CREATE TABLE annonces(
-        id                          Int  Auto_increment  NOT NULL ,
-        intitule                    Varchar (255) ,
-        domaine                     Varchar (255) ,
-        dateDebut                   Date NOT NULL ,
-        dateFin                     Date NOT NULL ,
-        description                 Varchar (1500) ,
-        salaire                     Varchar (255) ,
-        id_utilisateurs_entreprises Int ,
-        id_entreprises              Int ,
-        id_villes                   Int ,
-        id_typesContrat             Int
+        id              Int  Auto_increment  NOT NULL ,
+        intitule        Varchar (255) ,
+        domaine         Varchar (255) ,
+        dateDebut       Date NOT NULL ,
+        dateFin         Date NOT NULL ,
+        description     Varchar (1500) ,
+        salaire         Varchar (255) ,
+        id_entreprises  Int ,
+        id_villes       Int ,
+        id_typesContrat Int
 	,CONSTRAINT annonces_PK PRIMARY KEY (id)
 
-	,CONSTRAINT annonces_entreprises_FK FOREIGN KEY (id_utilisateurs_entreprises,id_entreprises) REFERENCES entreprises(id_utilisateurs,id)
+	,CONSTRAINT annonces_entreprises_FK FOREIGN KEY (id_entreprises) REFERENCES entreprises(id)
 	,CONSTRAINT annonces_villes0_FK FOREIGN KEY (id_villes) REFERENCES villes(id)
 	,CONSTRAINT annonces_typesContrat1_FK FOREIGN KEY (id_typesContrat) REFERENCES typesContrat(id)
 )ENGINE=InnoDB;
@@ -128,12 +125,11 @@ CREATE TABLE fichiers(
 #------------------------------------------------------------
 
 CREATE TABLE postuler(
-        id                        Int NOT NULL ,
-        id_utilisateurs_candidats Int NOT NULL ,
-        id_candidats              Int NOT NULL
-	,CONSTRAINT postuler_PK PRIMARY KEY (id,id_utilisateurs_candidats,id_candidats)
+        id           Int NOT NULL ,
+        id_candidats Int NOT NULL
+	,CONSTRAINT postuler_PK PRIMARY KEY (id,id_candidats)
 
 	,CONSTRAINT postuler_annonces_FK FOREIGN KEY (id) REFERENCES annonces(id)
-	,CONSTRAINT postuler_candidats0_FK FOREIGN KEY (id_utilisateurs_candidats,id_candidats) REFERENCES candidats(id_utilisateurs,id)
+	,CONSTRAINT postuler_candidats0_FK FOREIGN KEY (id_candidats) REFERENCES candidats(id)
 )ENGINE=InnoDB;
 
