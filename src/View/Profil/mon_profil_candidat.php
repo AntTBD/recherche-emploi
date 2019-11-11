09a9d669be959e3e9e3f393b3086596ad3046c8c
Marmorand <martinmorand31@gmail.com>
2019-11-11 17:19:48 +0100

    début formulaire candidat

--- src/View/Profil/mon_profil_candidat.php
+++ src/View/Profil/mon_profil_candidat.php
@@ -7,10 +7,25 @@
     </div>
     <div class="card-body text-dark">
         <form method='post' action="" onSubmit="return validate()">
+          <div class="form-group form-row">
+              <label class="col-sm-2 col-form-label" for="nom">Photo de Profil</label>
+              <div class="col-sm-10">
+                <div class="custom-file">
+                  <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                  <label class="custom-file-label" for="inputGroupFile01" data-browse="Partager">Indiquez la Photo que vous souhaitez utiliser</label>
+                </div>
+              </div>
+          </div>
             <div class="form-group form-row">
                 <label class="col-sm-2 col-form-label" for="nom">Nom</label>
                 <div class="col-sm-10">
-                    <input class="form-control" id="nom" type="text" name="nom" placeholder="Entrer le nom de votre entreprise" required>
+                    <input class="form-control" id="nom" type="text" name="nom" placeholder="Entrer votre Nom" required>
+                </div>
+            </div>
+            <div class="form-group form-row">
+                <label class="col-sm-2 col-form-label" for="prenom">Prenom</label>
+                <div class="col-sm-10">
+                    <input class="form-control" id="nom" type="text" name="nom" placeholder="Entrer votre Prénom" required>
                 </div>
             </div>
             <div class="form-group form-row">
@@ -25,78 +40,30 @@
                     <input class="form-control" type="tel" name="tel" id="tel" placeholder="Entrer votre numéro de téléphone">
                 </div>
             </div>
-            <div class="form-group">
-                <div class="form-row">
-                    <label class="col-sm-2 col-form-label" for="adresse">Adresse</label>
-                    <div class="col-sm-6">
-                        <input class="form-control" type="text" name="ville" id="ville" placeholder="Entrer votre ville">
-                    </div>
-                    <div class="col-sm-4">
-                        <input class="form-control" type="number" name="cp" id="cp" placeholder="Entrer votre code postal">
-                    </div>
+            <div class="form-group form-row">
+                <label class="col-sm-2 col-form-label" for="nom">Curriculum vitæ</label>
+                <div class="col-sm-10">
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                    <label class="custom-file-label" for="inputGroupFile01" data-browse="Partager">Sélectionnez votre CV</label>
+                  </div>
                 </div>
             </div>
             <div class="form-group form-row">
-                <label class="col-sm-2 col-form-label" for="siteInternet">Site Internet</label>
+                <label class="col-sm-2 col-form-label" for="nom">Lettre de motivation</label>
                 <div class="col-sm-10">
-                    <input class="form-control" type="url" name="siteInternet" id="siteInternet" placeholder="Entrer un lien vers votre site">
-                </div>
-            </div>
-            <div class="form-group form-row">
-                <label class="col-sm-2 col-form-label" for="description">Description</label>
-                <div class="col-sm-10">
-                    <textarea class="form-control" id="description" name="description" rows="4" placeholder="Descrir votre entreprise"></textarea>
-                </div>
-            </div>
-
-            <div class="form-group form-row">
-                <label class="col-sm-2 col-form-label" for="password">Mot de passe *</label>
-                <div class="col-sm-10">
-                    <input class="form-control" type="password" name="password" id="password" placeholder="Entrer un mot de passe" aria-describedby="btn_eye" maxlength="20" minlength="8" required>
-                    <small class="text-muted">
-                        Doit contenir entre 8 et 20 caractères.
-                    </small>
-                </div>
-            </div>
-            <div class="form-group form-row">
-                <label class="col-sm-2 col-form-label" for="passwordConfirm">Confirmer *</label>
-                <div class="col-sm-10">
-                    <input class="form-control" type="password" name="passwordConfirm" id="passwordConfirm" placeholder="Confirmer votre mot de passe" aria-describedby="btn_eye" maxlength="20" minlength="8" required>
-                    <small class="text-muted">
-                        Doit contenir entre 8 et 20 caractères.
-                    </small>
+                  <div class="custom-file">
+                    <input type="file" class="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01">
+                    <label class="custom-file-label" for="inputGroupFile01" data-browse="Partager">Sélectionnez votre lettre de motivation</label>
+                  </div>
                 </div>
             </div>
             <div class="form-group form-row">
                 <div class="col-sm-2"></div>
                 <div class="col-sm-10">
-                    <button class="btn btn-primary btn-success" type="submit">Créer mon compte</button>
-                    <button class="btn btn-primary btn-danger" type="reset">Annuler</button>
-                </div>
-            </div>
-            <div class="form-group form-row">
-                <div class="col-sm-2"></div>
-                <div class="col-sm-10 form-text">
-                    Vous avez déjà un compte? <a href="/index.php/connexion_entreprise"><span class="">Se connecter</span></a>
+                    <button class="btn btn-primary btn-success" type="submit">Modifier mes informations</button>
                 </div>
             </div>
         </form>
-        <div class="modal fade" id="modal" tabindex="-1" role="dialog" aria-labelledby="modalTitre" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="modalTitre">Attention</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Les mots de passe ne sont pas identique !
-                        <br>Veuillez mettre le même mot de passe</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
