fbb59019122ab05dc1e7618d568d698d00269ad8
Marmorand31 <47538734+Marmorand31@users.noreply.github.com>
2019-12-08 14:14:40 +0100

    Modification profil entrerpise

--- src/View/Profil/mon_profil_entreprise.php
+++ src/View/Profil/mon_profil_entreprise.php
@@ -11,15 +11,15 @@
               <label class="col-sm-2 col-form-label" for="nom">Logo de l'entreprise</label>
               <div class="col-sm-10">
                   <!--<div class="custom-file">
-                      <input type="file" class="custom-file-input" id="CV" name="CV" required>
-                      <label class="custom-file-label" for="CV">Choose file...</label>
+                      <input type="file" class="custom-file-input" id="Logo" name="Logo" required>
+                      <label class="custom-file-label" for="Logo">Choose file...</label>
                       <div class="invalid-feedback">Example invalid custom file feedback</div>
                   </div>-->
                 <div class="custom-file">
                   <input type="hidden" name="MAX_FILE_SIZE" value="100000">
                     <input type="file" name = "USERFILE">
-                    <input type="file" class="custom-file-input" id="CV" name="CV" aria-describedby="inputGroupFileAddon01">
-                    <label class="custom-file-label" for="CV" data-browse="Partager">Indiquez le logo que vous souhaitez utiliser</label>
+                    <input type="file" class="custom-file-input" id="Logo" name="Logo" aria-describedby="inputGroupFileAddon01">
+                    <label class="custom-file-label" for="Logo" data-browse="Partager">Indiquez le logo que vous souhaitez utiliser</label>
                 </div>
               </div>
           </div>
@@ -80,92 +80,11 @@
     </script>
 <!-- formulaire profil entreprise -->
 
-    <?php
-    if(isset($_FILES['CV']))
-    {
-      $dossier = 'upload/';
-      $fichier = basename($_FILES['CV']['name']);
-      $taille_maxi = 100000;
-      $taille = filesize($_FILES['CV']['tmp_name']);
-      $extensions = array('.png', '.gif', '.jpg', '.jpeg');
-      $extension = strrchr($_FILES['CV']['name'], '.');
-      //Verification du fichier uploadé
-      if(!in_array($extension, $extensions)) //Verification de l'extention
-      {
-        $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
-      }
-      if($taille>$taille_maxi) //Verification de la taille
-      {
-        $erreur = 'Le fichier est trop gros...';
-      }
-      if(!isset($erreur))
-      {
-        //On remplace les caractères spéciaux
-        $fichier = strtr($fichier,
-            'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ',
-            'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-            $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-            if(move_uploaded_file($_FILES['CV']['tmp_name'], $dossier . $fichier))
-            {
-              echo 'Upload effectué avec succès !';
-            }
-            else
-            {
-              echo 'Echec de l\'upload !';
-            }
-          }
-          else
-          {
-            echo $erreur;
-          }
-        }
-
-/*
-     //https://www.w3schools.com/php/php_file_upload.asp
-    $target_dir = "./upload/";
-    $target_file = $target_dir . basename($_FILES["CV"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["CV"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
-        $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["CV"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-        && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-        // if everything is ok, try to upload file
-    } else {
-        if (move_uploaded_file($_FILES["CV"]["tmp_name"], $target_file)) {
-            echo "The file ". basename( $_FILES["CV"]["name"]). " has been uploaded.";
-        } else {
-            echo "Sorry, there was an error uploading your file.";
-        }
-    }
-*/
-
-
-
-    ?>
+<script>
+    // https://www.w3schools.com/bootstrap4/bootstrap_forms_custom.asp#Custom%20File%20Upload
+    // Add the following code if you want the name of the file appear on select
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
+</script>
